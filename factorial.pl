f9b5aecd6d660555e9f0e65fa0e0b446b4678f70
Lahari158 <klahari.cpl@gmail.com>
2025-11-22 10:47:29 +0530

    Add files via upload

---
+++ factorial.pl
@@ -0,0 +1,10 @@
+% factorial(N, Result).
+% Base case:
+factorial(0, 1).
+
+% Recursive case:
+factorial(N, R) :-
+    N > 0,
+    N1 is N - 1,
+    factorial(N1, R1),
+    R is N * R1.
---
+++ fibonacci.pl
@@ -0,0 +1,13 @@
+% fibonacci(N, Result).
+% Base cases:
+fibonacci(0, 0).
+fibonacci(1, 1).
+
+% Recursive case:
+fibonacci(N, R) :-
+    N > 1,
+    N1 is N - 1,
+    N2 is N - 2,
+    fibonacci(N1, R1),
+    fibonacci(N2, R2),
+    R is R1 + R2.
